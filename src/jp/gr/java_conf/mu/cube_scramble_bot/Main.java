b84a2cfc0d5149eb56c2995b20d10b67afa2f177
masaru <masaru@192.168.1.2>
2017-05-14 20:33:54 +0900

    固定文言のツイート

--- src/jp/gr/java_conf/mu/cube_scramble_bot/Main.java
+++ src/jp/gr/java_conf/mu/cube_scramble_bot/Main.java
@@ -4,6 +4,12 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Main implements RequestHandler<Object, Object> {
 	private LambdaLogger logger;
 
@@ -11,6 +17,23 @@ public class Main implements RequestHandler<Object, Object> {
 		logger = context.getLogger();
 		logger.log("Input: " + input);
 
+		// 環境変数から各種キーを設定
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true).setOAuthConsumerKey(System.getenv("twitter4j_oauth_consumerKey"))
+				.setOAuthConsumerSecret(System.getenv("twitter4j_oauth_consumerSecret"))
+				.setOAuthAccessToken(System.getenv("twitter4j_oauth_accessToken"))
+				.setOAuthAccessTokenSecret(System.getenv("twitter4j_oauth_accessTokenSecret"));
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+
+		try {
+			Status status = twitter.updateStatus("テスト");
+			logger.log("Successfully updated the status to [" + status.getText() + "].");
+		} catch (TwitterException e) {
+			logger.log(e.getErrorMessage());
+			e.printStackTrace();
+			return null;
+		}
 		return null;
 	}
 }
