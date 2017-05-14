5196e7a0410952898a620a1be191880421a437aa
masaru <masaru@192.168.1.2>
2017-05-14 19:19:29 +0900

    雛形を作成

--- src/jp/gr/java_conf/mu/cube_scramble_bot/Main.java
+++ src/jp/gr/java_conf/mu/cube_scramble_bot/Main.java
@@ -1,10 +1,16 @@
 package jp.gr.java_conf.mu.cube_scramble_bot;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Test implements RequestHandler<Object, Object> {
+public class Main implements RequestHandler<Object, Object> {
+	private LambdaLogger logger;
+
 	public Object handleRequest(Object input, Context context) {
+		logger = context.getLogger();
+		logger.log("Input: " + input);
+
 		return null;
 	}
 }
