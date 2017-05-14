a2d49bdff03f5cced17785d3b6a0c4fbff5b624c
masaru <masaru@192.168.1.2>
2017-05-14 18:52:25 +0900

    プロジェクト生成

---
+++ cube_scramble_bot/src/jp/gr/java_conf/mu/cube_scramble_bot/Test.java
@@ -0,0 +1,10 @@
+package jp.gr.java_conf.mu.cube_scramble_bot;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Test implements RequestHandler<Object, Object> {
+	public Object handleRequest(Object input, Context context) {
+		return null;
+	}
+}
