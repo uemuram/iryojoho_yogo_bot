88f188524ebc98f262b7570d5ae949818c4c511e
masaru <masaru@192.168.1.2>
2017-05-14 23:19:12 +0900

    スクランブルをツイートするように修正

--- src/jp/gr/java_conf/mu/cube_scramble_bot/Main.java
+++ src/jp/gr/java_conf/mu/cube_scramble_bot/Main.java
@@ -26,14 +26,62 @@ public class Main implements RequestHandler<Object, Object> {
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		Twitter twitter = tf.getInstance();
 
+		// スクランブル生成
+		String scramble = generateScramble(20);
+		logger.log("スクランブル: " + scramble);
+
+		// ツイート
 		try {
-			Status status = twitter.updateStatus("テスト");
+			Status status = twitter.updateStatus(scramble);
 			logger.log("Successfully updated the status to [" + status.getText() + "].");
 		} catch (TwitterException e) {
+			logger.log("ツイート失敗");
 			logger.log(e.getErrorMessage());
-			e.printStackTrace();
-			return null;
 		}
+
 		return null;
+	}
+
+	// スクランブルを生成
+	private String generateScramble(int l) {
+		String faces[] = { "U", "D", "R", "L", "F", "B" };
+		String options[] = { "", "'", "2" };
+		String scramble = "";
+		int beforeFace = -1;
+		int before2Face = -1;
+		int currentFace;
+		for (int i = 0; i < l; i++) {
+			// 回す面を決める。1つ前と同じ面を選ばないようにする
+			do {
+				currentFace = randomN(6);
+			} while (!faceCheck(currentFace, beforeFace, before2Face));
+			scramble += (faces[currentFace] + options[randomN(3)]);
+			if (i < l - 1) {
+				scramble += " ";
+			}
+			// 2つ前、1つ前の手順を記録
+			before2Face = beforeFace;
+			beforeFace = currentFace;
+		}
+		return scramble;
+	}
+
+	// n種類(0～n-1)の乱数を生成
+	private int randomN(int n) {
+		return (int) (Math.random() * n);
+	}
+
+	// 回す面のチェック
+	private boolean faceCheck(int current, int before, int before2) {
+		// 1つ前と同じ面は回さない
+		if (current == before) {
+			return false;
+		}
+		// 同じ面 -> 対面 -> 同じ面 の順の回転はNG(例: U2, D, U')
+		if (current == before2
+				&& ((current % 2 == 0 && current - before == -1) || (current % 2 == 1 && current - before == 1))) {
+			return false;
+		}
+		return true;
 	}
 }
