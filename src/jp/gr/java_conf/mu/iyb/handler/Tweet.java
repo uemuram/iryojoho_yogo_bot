6c13397b83d0105896028fdf155c61dbb41f66fc
masaru <masaru@192.168.1.2>
2018-08-01 23:14:48 +0900

    pom整備

--- src/jp/gr/java_conf/mu/iyb/handler/Tweet.java
+++ src/jp/gr/java_conf/mu/iyb/handler/Tweet.java
@@ -27,49 +27,49 @@ public class Tweet implements RequestHandler<Object, Object> {
 		logger = context.getLogger();
 		logger.log("Input: " + input);
 
-		// DynamoDB利用準備
-		DynamoDBUtil dynamoDBUtil = new DynamoDBUtil(logger);
-
-		// Twitter利用準備
-		// 環境変数から各種キーを設定
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true).setOAuthConsumerKey(System.getenv("twitter4j_oauth_consumerKey"))
-				.setOAuthConsumerSecret(System.getenv("twitter4j_oauth_consumerSecret"))
-				.setOAuthAccessToken(System.getenv("twitter4j_oauth_accessToken"))
-				.setOAuthAccessTokenSecret(System.getenv("twitter4j_oauth_accessTokenSecret"));
-		Configuration configuration = cb.build();
-		TwitterFactory tf = new TwitterFactory(configuration);
-		Twitter twitter = tf.getInstance();
-
-		// ツイート文言を生成
-		String scramble = generateScramble(20);
-		logger.log("スクランブル: " + scramble);
-		String tweetText = scramble + "\n\n※タイム(秒)を何回か返信すると、集計して通知します。";
-
-		// ツイート
-		Status status;
-		try {
-			status = twitter.updateStatus(tweetText);
-			logger.log("---------------------------------------------------------------------");
-			logger.log("ツイート内容:" + status.getText());
-			logger.log("ツイートID:" + status.getId() + "");
-			logger.log("ツイート生成日時:" + status.getCreatedAt() + "");
-		} catch (TwitterException e1) {
-			logger.log("ツイート失敗 : " + e1.getErrorMessage());
-			throw new RuntimeException(e1);
-		}
-
-		// ツイート時刻を取得
-		LocalDateTime localDateTime = LocalDateTime.parse(status.getCreatedAt().toString(),
-				DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy"));
-		String createdAt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(localDateTime);
-
-		// ツイート内容をDBに登録
-		Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
-		item.put("id", new AttributeValue().withS(status.getId() + ""));
-		item.put("scramble", new AttributeValue().withS(scramble));
-		item.put("tweet_date", new AttributeValue().withS(createdAt));
-		dynamoDBUtil.putItem(TABLE_NAME_SCRAMBLE, item);
+//		// DynamoDB利用準備
+//		DynamoDBUtil dynamoDBUtil = new DynamoDBUtil(logger);
+//
+//		// Twitter利用準備
+//		// 環境変数から各種キーを設定
+//		ConfigurationBuilder cb = new ConfigurationBuilder();
+//		cb.setDebugEnabled(true).setOAuthConsumerKey(System.getenv("twitter4j_oauth_consumerKey"))
+//				.setOAuthConsumerSecret(System.getenv("twitter4j_oauth_consumerSecret"))
+//				.setOAuthAccessToken(System.getenv("twitter4j_oauth_accessToken"))
+//				.setOAuthAccessTokenSecret(System.getenv("twitter4j_oauth_accessTokenSecret"));
+//		Configuration configuration = cb.build();
+//		TwitterFactory tf = new TwitterFactory(configuration);
+//		Twitter twitter = tf.getInstance();
+//
+//		// ツイート文言を生成
+//		String scramble = generateScramble(20);
+//		logger.log("スクランブル: " + scramble);
+//		String tweetText = scramble + "\n\n※タイム(秒)を何回か返信すると、集計して通知します。";
+//
+//		// ツイート
+//		Status status;
+//		try {
+//			status = twitter.updateStatus(tweetText);
+//			logger.log("---------------------------------------------------------------------");
+//			logger.log("ツイート内容:" + status.getText());
+//			logger.log("ツイートID:" + status.getId() + "");
+//			logger.log("ツイート生成日時:" + status.getCreatedAt() + "");
+//		} catch (TwitterException e1) {
+//			logger.log("ツイート失敗 : " + e1.getErrorMessage());
+//			throw new RuntimeException(e1);
+//		}
+//
+//		// ツイート時刻を取得
+//		LocalDateTime localDateTime = LocalDateTime.parse(status.getCreatedAt().toString(),
+//				DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy"));
+//		String createdAt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(localDateTime);
+//
+//		// ツイート内容をDBに登録
+//		Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
+//		item.put("id", new AttributeValue().withS(status.getId() + ""));
+//		item.put("scramble", new AttributeValue().withS(scramble));
+//		item.put("tweet_date", new AttributeValue().withS(createdAt));
+//		dynamoDBUtil.putItem(TABLE_NAME_SCRAMBLE, item);
 
 		return null;
 	}
